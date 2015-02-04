bf855e0a214e4ae81718d881fba8364d961116b3
Francesco Siddi <francesco.siddi@gmail.com>
2015-02-04 02:02:21 +0100

    Added manage.py

---
+++ attract/manage.py
@@ -0,0 +1,15 @@
+from application import app
+from application import db
+from flask.ext.script import Manager
+# from flask.ext.migrate import Migrate
+# from flask.ext.migrate import MigrateCommand
+
+# migrate = Migrate(app, db)
+manager = Manager(app)
+# manager.add_command('db', MigrateCommand)
+
+@manager.command
+def create_all_tables():
+    db.create_all()
+
+manager.run()
