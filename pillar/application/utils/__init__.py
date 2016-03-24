79918c26b684212958e1b8e58f27aee0c49cdc01
Francesco Siddi <francesco.siddi@gmail.com>
2016-03-24 15:04:27 +0100

    PEP8 formatting

--- pillar/application/utils/__init__.py
+++ pillar/application/utils/__init__.py
@@ -2,8 +2,9 @@ import copy
 
 
 def remove_private_keys(document):
-    """Removes any key that starts with an underscore, returns result as new dictionary."""
-
+    """Removes any key that starts with an underscore, returns result as new
+    dictionary.
+    """
     patch_info = copy.deepcopy(document)
     for key in list(patch_info.keys()):
         if key.startswith('_'):
