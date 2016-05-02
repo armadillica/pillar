681754eade356e2313641acd726ce333bac8724b
Sybren A. Stüvel <sybren@stuvel.eu>
2016-05-02 11:12:19 +0200

    Removed some obsolete unit tests.

--- tests/test_encoding.py
+++ tests/test_encoding.py
@@ -17,15 +17,6 @@ class ZencoderNotificationTest(AbstractPillarTest):
                                     headers={'X-Zencoder-Notification-Secret': 'koro'})
         self.assertEqual(401, resp.status_code)
 
-    def test_good_secret_missing_file(self):
-        with self.app.test_request_context():
-            secret = self.app.config['ZENCODER_NOTIFICATIONS_SECRET']
-            resp = self.client.post('/encoding/zencoder/notifications',
-                                    data=json.dumps({'job': {'id': 'koro-007'}}),
-                                    headers={'X-Zencoder-Notification-Secret': secret,
-                                             'Content-Type': 'application/json'})
-        self.assertEqual(200, resp.status_code)
-
     def test_good_secret_existing_file(self):
         self.ensure_file_exists(file_overrides={
             'processing': {'backend': 'zencoder',
