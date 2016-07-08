f6eab4878f6227ab32e162150800cd784365ad59
Sybren A. Stüvel <sybren@stuvel.eu>
2016-07-08 12:08:01 +0200

    Log ensure_valid_link() stuff to separate logger.

--- pillar/application/modules/file_storage.py
+++ pillar/application/modules/file_storage.py
@@ -335,6 +335,8 @@ def before_returning_files(response):
 def ensure_valid_link(response):
     """Ensures the file item has valid file links using generate_link(...)."""
 
+    # Log to function-specific logger, so we can easily turn it off.
+    log = logging.getLogger('%s.ensure_valid_link')
     # log.debug('Inspecting link for file %s', response['_id'])
 
     # Check link expiry.
