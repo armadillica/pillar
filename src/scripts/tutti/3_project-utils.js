91e3ec659f68c9630eca6e9ed089c8c2e3fc1dd5
Sybren A. Stüvel <sybren@stuvel.eu>
2016-09-23 10:12:57 +0200

    Added ProjectUtils.projectUrl()

--- src/scripts/tutti/3_project-utils.js
+++ src/scripts/tutti/3_project-utils.js
@@ -3,6 +3,7 @@ ProjectUtils = {
     nodeId: function() { return document.body.dataset.nodeId; },
     parentNodeId: function() { return document.body.dataset.parentNodeId; },
     projectId: function() { return document.body.dataset.projectId; },
+    projectUrl: function() { return document.body.dataset.projectUrl; },
     isProject: function() { return document.body.dataset.isProject === 'true'; },
     nodeType: function() { return document.body.dataset.nodeType; },
     isModified: function() { return document.body.dataset.isModified === 'true'; },
