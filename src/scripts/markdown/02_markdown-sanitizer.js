9ccf4474bc1c725e8c9151a2c6dd36fde6eb3768
Francesco Siddi <francesco.siddi@gmail.com>
2017-03-28 17:54:10 +0200

    Fix for missing tag in Markdown validator

--- src/scripts/markdown/02_markdown-sanitizer.js
+++ src/scripts/markdown/02_markdown-sanitizer.js
@@ -31,9 +31,10 @@
 
     // <img src="url..." optional width  optional height  optional alt  optional title
     var img_white = /^(<img\ssrc="(https?:\/\/|\/)[-A-Za-z0-9+&@#\/%?=~_|!:,.;\(\)*[\]$]+"(\swidth="\d{1,3}")?(\sheight="\d{1,3}")?(\salt="[^"<>]*")?(\stitle="[^"<>]*")?\s?\/?>)$/i;
+    var video_white =  /<video(.*?)>/;
 
     function sanitizeTag(tag) {
-        if (tag.match(basic_tag_whitelist) || tag.match(a_white) || tag.match(img_white) || tag.match(iframe_youtube) || tag.match(iframe_vimeo) || tag.match(iframe_soundcloud)) {
+        if (tag.match(basic_tag_whitelist) || tag.match(a_white) || tag.match(img_white) || tag.match(iframe_youtube) || tag.match(iframe_vimeo) || tag.match(iframe_soundcloud) || tag.match(video_white)) {
             return tag;
         } else {
             return "";
