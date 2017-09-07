7dbd0c789636229624748e71c0d9eaa1df8946ee
Sybren A. Stüvel <sybren@stuvel.eu>
2017-09-07 15:52:57 +0200

    Exclude Flamenco task logs when dumping the database.

--- backup-db.sh
+++ backup-db.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -ex
 
-mongodump -h localhost:27018 -d cloud --out dump/$(date +'%Y-%m-%d-%H%M') --excludeCollection tokens
+mongodump -h localhost:27018 -d cloud --out dump/$(date +'%Y-%m-%d-%H%M') --excludeCollection tokens --excludeCollection flamenco_task_logs
