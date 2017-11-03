d0f10779f9a70cbeec640d79d428845f4c9bbb4b
Sybren A. Stüvel <sybren@stuvel.eu>
2017-11-03 14:32:13 +0100

    Added useful 'string' alias to attrs_extra

--- pillar/attrs_extra.py
+++ pillar/attrs_extra.py
@@ -1,8 +1,11 @@
 """Extra functionality for attrs."""
 
+import functools
 import logging
 
 import attr
+
+string = functools.partial(attr.ib, validator=attr.validators.instance_of(str))
 
 
 def log(name):
