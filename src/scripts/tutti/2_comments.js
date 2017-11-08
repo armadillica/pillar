ac9aa59924062e34c22214d233b65c7cf644373d
Pablo Vazquez <venomgfx@gmail.com>
2017-11-08 22:49:56 +0100

    Comments: When editing, resize the textarea
    Minor tweaks:
    * Remove the 'editing' class after cancel/save
    * Style <code>, <pre>, etc tags

--- src/scripts/tutti/2_comments.js
+++ src/scripts/tutti/2_comments.js
@@ -235,7 +235,10 @@ function commentEditCancel(clicked_item, reload_comment) {
 	return loadComment(comment_id, {'properties.content': 1})
 	.done(function(data) {
 		var comment_html = data['properties']['content_html'];
-		comment_container.find('.comment-body span').html(comment_html);
+		comment_container
+			.find('.comment-body span')
+			.removeClass('editing')
+			.html(comment_html);
 	})
 	.fail(function(xhr) {
 		if (console) console.log('Error fetching comment: ', xhr);
