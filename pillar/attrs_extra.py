a23e063002c5ab5b9cd9d793755c981aec8e90dc
Sybren A. Stüvel <sybren@stuvel.eu>
2019-01-04 12:45:47 +0100

    Don't use attr.ib to declare a logger
    This doesn't work well when overriding in subclasses; it keeps using the
    superclass logger. Simply returning a logger fixes this.

--- pillar/attrs_extra.py
+++ pillar/attrs_extra.py
@@ -9,12 +9,8 @@ string = functools.partial(attr.ib, validator=attr.validators.instance_of(str))
 
 
 def log(name):
-    """Returns a logger attr.ib
+    """Returns a logger
 
     :param name: name to pass to logging.getLogger()
-    :rtype: attr.ib
     """
-    return attr.ib(default=logging.getLogger(name),
-                   repr=False,
-                   hash=False,
-                   cmp=False)
+    return logging.getLogger(name)
